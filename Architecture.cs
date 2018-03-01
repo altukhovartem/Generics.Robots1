722d1e7c3ae9b74f5285e07bfd02f7c1a7349ff8
aaltukhov <aaltukhov@acumatica.com>
2018-03-01 19:42:32 +0300

    1 commit

---
+++ Architecture.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Generics.Robots
+{
+
+	public interface IRobotic<out T>
+	{
+		T GetCommand();
+	}
+
+	public abstract class RobotAI<T> : IRobotic<T>
+	{
+		public abstract T GetCommand();
+	}
+
+	public class ShooterAI<T> : RobotAI<T>
+		where T : ShooterCommand
+	{
+		int counter = 1;
+
+		public override T GetCommand()
+		{
+			return ShooterCommand.ForCounter(counter++) as T;
+		}
+	}
+
+	public class BuilderAI<T> : RobotAI<T>
+				where T : BuilderCommand
+	{
+		int counter = 1;
+		public override T GetCommand()
+		{
+			return BuilderCommand.ForCounter(counter++) as T;
+		}
+	}
+
+	public interface IDevicable<in T>
+	{
+		string ExecuteCommand(T command);
+	}
+
+	public abstract class Device<T> : IDevicable<T>
+	{
+		public abstract string ExecuteCommand(T command);
+	}
+
+	public class Mover<T> : Device<T>
+	{
+		public override string ExecuteCommand(T _command)
+		{
+			var command = _command as IMoveCommand;
+			if (command == null)
+				throw new ArgumentException();
+			return $"MOV {command.Destination.X}, {command.Destination.Y}";
+		}
+	}
+
+
+
+	public class Robot<T>
+	{
+		RobotAI<T> ai;
+		Device<T> device;
+
+		public Robot(RobotAI<T> ai, Device<T> executor)
+		{
+			this.ai = ai;
+			this.device = executor;
+		}
+
+		public IEnumerable<string> Start(int steps)
+		{
+			for (int i = 0; i < steps; i++)
+			{
+				var command = ai.GetCommand();
+				if (command == null)
+					break;
+				yield return device.ExecuteCommand(command);
+			}
+
+		}
+
+		public static Robot<T> Create(RobotAI<T> ai, Device<T> executor)
+		{
+			return new Robot<T>(ai, executor);
+		}
+	}
+
+
+}
---
+++ Architecture_Should.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics.Robots
+{
+    [TestFixture]
+    public class Architecture_Should
+    {
+        [Test]
+        public void BeCorrectForShooter()
+        {
+            var robot = Robot<ShooterCommand>.Create(new ShooterAI<ShooterCommand>(), new Mover<ShooterCommand>());
+            var result = robot.Start(5);
+            var dueResult = Enumerable.Range(1, 5).Select(z => $"MOV {z * 2}, {z * 3}");
+            CollectionAssert.AreEqual(dueResult, result);
+        }
+
+        [Test]
+        public void BeCorrectForBuilder()
+        {
+            var robot = Robot<BuilderCommand>.Create(new BuilderAI<BuilderCommand>(), new Mover<BuilderCommand>());
+            var result = robot.Start(5).ToArray();
+            var dueResult = Enumerable.Range(1, 5).Select(z => $"MOV {z}, {z}");
+            CollectionAssert.AreEqual(dueResult, result);
+        }
+    }
+}
